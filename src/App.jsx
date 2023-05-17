a867b72a26543f1f64e0d7535f5d11542b1aa371
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-05-17 22:29:44 +0600

    connecting to the gthub

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+
+
+    </>
+  )
+}
+
+export default App
